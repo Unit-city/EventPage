e96696b07c659e4b44e083fae77af28e736c1eda
Unit-city <61819933+Unit-city@users.noreply.github.com>
2020-03-11 11:50:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-var code = document.getElementById('code');
-var button = document.getElementById('btn');
-code.focus();
-
-button.onclick = function(){
-  if(code.value==='C05671'){
-    window.location.href='1.html';
-  }
-  else{
-    window.location.href='error.html';
-  }
-}
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('2 0=3.4(\'0\');2 5=3.4(\'a\');0.b();5.c=d(){e(0.f===\'g\'){6.7.8=\'1.9\'}h{6.7.8=\'i.9\'}}',19,19,'code||var|document|getElementById|button|window|location|href|html|btn|focus|onclick|function|if|value|C05671|else|error'.split('|'),0,{}))
